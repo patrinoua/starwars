2c1ace034eba9d4c9b9eb09f60659718d4268d8d
Mona Lisa <patrinoua@gmail.com>
2018-12-22 02:19:36 +0200

    edit server

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ const port = process.env.PORT || 5000
 
 app.use(bodyParser.json())
 
+app.get('/', function(req, res) {
+  res.sendStatus(200)
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}`))
